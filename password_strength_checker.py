629abf458cbb59ea6e4f74e636d51e651a3fa1cb
Darren <darrenmoral@ymail.com>
2025-05-15 22:44:17 -0500

    Basic code to start this program

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,1 +1,8 @@
+user_input = input("What's your password? >:) ")
 
+print(user_input)
+
+if len(user_input) < 8:
+    print("Your password isn't long enough.")
+else:
+    print("That is a strong password.")
